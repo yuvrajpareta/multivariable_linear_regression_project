2efd93fbc311ec3750d1eaf6742a0d8888bb2789
yuvrajpareta <83811857+yuvrajpareta@users.noreply.github.com>
2022-07-28 23:24:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+
+import pickle
+
+
+application = Flask(__name__)
+model = pickle.load(open('house1-price.pkl','rb')) 
+
+
+@application.route('/')
+def home():
+  
+    return render_template("index.html")
+  
+@application.route('/predict',methods=['GET'])
+def predict():
+  r1= float(request.args.get('exp'))
+  r2= float(request.args.get('bed'))
+  r3= float(request.args.get('bath'))
+  r4= float(request.args.get('off'))
+  r5= float(request.args.get('brick'))
+  r6= float(request.args.get('neigh'))
+  
+    
+  prediction = model.predict([[r1,r2,r3,r4,r5,r6]]) 
+
+    
+    
+    
+
+    
+    
+        
+  return render_template('index.html', prediction_text='Regression Model  has predicted Price of House for given features is : {}'.format(prediction))
+application.run()
